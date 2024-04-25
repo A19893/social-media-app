9d0a9ed50c23264311c45a170e129aaa0fd4841c
Yash Arora <yash.1138@zenmonk.tech>
2024-04-25 12:37:16 +0530

    Implemented Signup and Login Page functionality

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -2,34 +2,47 @@ import { useTheme } from '@emotion/react'
 import { Box, Typography, useMediaQuery } from '@mui/material';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import Form from './Form';
 
 const LoginPage = () => {
-  const theme= useTheme();
+  const theme = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-  const dark = theme.palette.dark;
-  const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const navigate = useNavigate();
 
-  return <Box>
-    <Box width='100%' backgroundColor= {theme.palette.background.alt} p="1rem 6%" textAlign="center">
-    <Typography
-      fontWeight="bold"
-      fontSize= "32px"
-      color= "primary"
-      onClick={()=> navigate("/home")}
-      sx={{
-        "&:hover": {
-          color: primaryLight,
-          cursor: "pointer"
-        }
-      }}
+  return (
+    <Box>
+      <Box width='100%' backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer"
+            }
+          }}
+        >
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
       >
-      Sociopedia
-    </Typography>
+        <Typography fontWeight="500" variant='h5' sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
     </Box>
-    
-  </Box>
+  )
 }
 
 export default LoginPage
